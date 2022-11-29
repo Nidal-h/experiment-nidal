151776a0f5f078d8f352333b882ec7b582228dd9
NIDAL HADROU <nidal@DESKTOP-F2CC40J.com>
2022-11-29 10:19:31 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,76 @@
+from typing import List
+
+import databases
+
+import sqlalchemy
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+DATABASE_URL = "postgresql://xnfajmqycpswkk:cb5b9e5b905cc9fd2b5605233b11cf6084f0b79a01e1df8869be80d70d08c4e9@ec2-44-195-100-240.compute-1.amazonaws.com:5432/dcig9h8hsmn9if"
+
+database = databases.Database(DATABASE_URL)
+
+
+metadata = sqlalchemy.MetaData()
+
+
+
+notes = sqlalchemy.Table(
+
+    "notes",
+
+    metadata,
+
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+
+    sqlalchemy.Column("text", sqlalchemy.String),
+
+    sqlalchemy.Column("completed", sqlalchemy.Boolean),
+
+)
+
+
+
+engine = sqlalchemy.create_engine(
+    DATABASE_URL
+)
+metadata.create_all(engine)
+
+
+class NoteIn(BaseModel):
+    text: str
+    completed: bool
+
+
+class Note(BaseModel):
+    id: int
+    text: str
+    completed: bool
+
+
+app = FastAPI()
+
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
+
+@app.get("/notes/", response_model=List[Note])
+async def read_notes():
+    query = notes.select()
+    return await database.fetch_all(query)
+
+
+@app.post("/notes/", response_model=Note)
+async def create_note(note: NoteIn):
+    query = notes.insert().values(text=note.text, completed=note.completed)
+    last_record_id = await database.execute(query)
+    return {**note.dict(), "id": last_record_id}
